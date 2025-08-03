6fa67de3f66ee67c7c43897f00c99bf7063f8509
Ilechukwu Francis C <ilechukwufran6x@gmail.com>
2025-08-03 21:28:57 +0100

    Include a Redux for global state management

---
+++ alx-project-0x06/components/layouts/Layout.tsx
@@ -0,0 +1,10 @@
+import Header from "./Header";
+
+const Layout = ({ children }: { children: React.ReactNode }) => (
+  <>
+    <Header />
+    <main>{children}</main>
+  </>
+);
+
+export default Layout;
