969745997d65eef9acb38a1b458daa90ed486a0d
Ilechukwu Francis C <ilechukwufran6x@gmail.com>
2025-08-03 21:36:32 +0100

    Include a Redux for global state management

--- alx-project-0x06/components/layouts/Header.tsx
+++ alx-project-0x06/components/layouts/Header.tsx
@@ -5,28 +5,36 @@ import { RootState } from "@/store/store";
 import { useSelector } from "react-redux";
 
 const Header: React.FC = () => {
-  const pathname = usePathname();
-  const count = useSelector((state: RootState) => state.counter.value);
-  const isCounterApp = pathname === "/counter-app";
+
+  const pathname = usePathname()
+  const count = useSelector((state: RootState) => state.counter.value)
+
 
   return (
     <header className="fixed w-full bg-white shadow-md">
       <div className="container mx-auto flex justify-between items-center py-6 px-4 md:px-8">
-        <Link href="/" legacyBehavior>
-          <a className="text-3xl md:text-5xl font-bold text-gray-800 tracking-tight">
-            Splash App
-          </a>
+        <Link href="/" className="text-3xl md:text-5xl font-bold text-gray-800 tracking-tight">
+          Splash App
         </Link>
 
+        {/* Button Group */}
         <div className="flex gap-4">
-          {!isCounterApp ? (
-            <>
-              <Button buttonLabel="Sign In" buttonBackgroundColor="red" />
-              <Button buttonLabel="Sign Up" buttonBackgroundColor="blue" />
-            </>
-          ) : (
-            <p className="font-semibold text-lg">Current count : {count}</p>
-          )}
+          {
+            !["/counter-app"].includes(pathname) ? (
+              <>
+              <Button
+            buttonLabel="Sign In"
+            buttonBackgroundColor="red"
+          />
+          <Button
+            buttonLabel="Sign Up"
+            buttonBackgroundColor="blue"
+          /></>
+            ) : (
+              <p className=" font-semibold text-lg">Current count : {count}</p>
+            )
+          }
+
         </div>
       </div>
     </header>
