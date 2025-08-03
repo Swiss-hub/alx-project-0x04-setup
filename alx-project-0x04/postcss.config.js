b1613b316cbe06074c0697f01e516be8092cf007
Ilechukwu Francis C <ilechukwufran6x@gmail.com>
2025-08-03 19:59:03 +0100

    Adding a counter app to our platform with useState Hook

---
+++ alx-project-0x04/postcss.config.js
@@ -0,0 +1,7 @@
+// filepath: c:\Users\ABC\Desktop\ALX\alx-project-0x04-setup\alx-project-0x04\postcss.config.js
+module.exports = {
+  plugins: {
+    '@tailwindcss/postcss': {},
+    autoprefixer: {},
+  },
+}
---
+++ alx-project-0x04/tailwind.config.js
@@ -0,0 +1,12 @@
+// filepath: c:\Users\ABC\Desktop\ALX\alx-project-0x04-setup\alx-project-0x04\tailwind.config.js
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
